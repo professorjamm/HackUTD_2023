ae67dac612190aa74d8c8ee396cc7e3ce7586fd3
bluebrowney <zaaimrahman.comp@gmail.com>
2023-11-05 08:10:33 -0600

    Creating styles for the application

--- src/App.js
+++ src/App.js
@@ -9,6 +9,17 @@ let stage = 0;
 function App() {
 
   //Application Constants
+  const definitions = [
+      "Gross monthly income refers to how much money an individual earns before deductions.", 
+      "Credit Card payments refers to your average monthly credit card balance statement",
+      "Car Payments refer to your average monthly costs towards paying off your car",
+      "Student Loan Payments refer to your monthly payments towards your student debt",
+      "Appraised Value refers to the value of the house you are looking to purchase",
+      "Down Payment refers to the initial payment made when your house is bought on credit",
+      "Loan Amount refers to the amount of money you are being loaned to purchase the house",
+      "Monthly Mortage Payment refers to the monthly payments you will pay for the house",
+      "Credit Score refers to a numerical rating representing the perceived ability of a person or organization to fulfill their financial commitments",
+    ];
   const titleVals = ['Housing Eligibility Assistant', 'Data Input', 'Approved', 'Denied'];
   const buttonNameVals = ['Next', 'Submit', 'Return'];
   const contentVals = [
@@ -18,9 +29,51 @@ function App() {
           </p>
       </div>),
       (<div id="stage2">
+          <form action="" method="POST" id="data">
+              
+              <div>
+                <label for="grossIncome" placeholder="$00.00"><abbr title={definitions[0]}>Gross Monthly Income</abbr></label>
+                <input type="text" id="grossIncome"/>
+              </div>
+              
+            <div>              
+                  <label for="ccPayment" placeholder="$00.00"><abbr title={definitions[1]}>Credit Card Payment</abbr></label>
+                  <input type="text" id="ccPayment"/>
+              </div>
+              <div>
+                  <label for="carPayment" placeholder="$00.00"><abbr title={definitions[2]}>Car Payment</abbr></label>
+                  <input type="text" id="carPayment"/>
+              </div>
+              <div>
+                  <label for="slPaymnet" placeholder="$00.00"><abbr title={definitions[3]}>Student Loan Payment</abbr></label>
+                  <input type="text" id="slPayment"/>
+              </div>
+              <div>
+                  <label for="appValue" placeholder="$00.00"><abbr title={definitions[4]}>Appraised Value</abbr></label>
+                  <input type="text" id="appValue"/>
+              </div>
+              <div>
+                  <label for="dPayment" placeholder="$00.00"><abbr title={definitions[5]}>Down Payment</abbr></label>
+                  <input type="text" id="dPayment"/>
+              </div>
+              <div>
+                  <label for="loanAmount" placeholder="$00.00"><abbr title={definitions[6]}>Loan Amount</abbr></label>
+                  <input type="text" id="loanAmount"/>
+              </div>
+              <div>
+                  <label for="mmPayment" placeholder="$00.00"><abbr title={definitions[7]}>Monthly Mortgage Payment</abbr></label>
+                  <input type="text" id="mmPayment"/>
+              </div>
+              <div>
+                  <label for="creditScore" placeholder="$00.00"><abbr title={definitions[8]}>Credit Score</abbr></label>
+                  <input type="text" id="creditScore"/>
+              </div>
+          </form>
+      </div>),
+      (<div id="stage3a">
 
       </div>),
-      (<div id="stage3">
+      (<div id="stage3d">
 
       </div>)];
 
@@ -35,6 +88,26 @@ function App() {
   const callback = () => {
 
         stage++;
+
+        if(stage === 2) {
+
+            let el = document.getElementById('data');
+            let data = [];
+
+            for(let i = 0; i < 9; i++) {
+
+                data[i] = el.children[i].children[1].value;
+
+            }
+
+            //send data here
+            data = data.map((val) => {
+                return Number(val);
+            });
+
+            let test;
+
+        }
  
         if(stage > 2) {
 
